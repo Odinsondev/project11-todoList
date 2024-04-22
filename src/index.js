de1e852f4b2263bf07848adbd1170501b128542e
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-04-22 01:35:08 -0700

    Feat: set up project directory

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './normalize.css';
+import './style.css';
+//import other stylesheets
+
+/* import functionName from './javascriptFile'; */
+
+/* import pictureName from './imagedDirectory/imageName'; */
+
+
+//initialize
+function init() {
+}
+/* init(); */
+
+//cache DOM
+
+//bind events
+
+//functions
